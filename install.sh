d5e1a69e7e229f10a336abab4faf8a4fd40b23ab
fredriki <17081472+fredriki@users.noreply.github.com>
2022-11-04 15:10:36 +0100

    Changed how env is managed
    If no special .env is needed (i.e. only MYDOMAIN and TZ) a container does no longer need a install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
-set_common_env ()
+set_env ()
 {
     read -p 'Domain name: ' domain
     echo MYDOMAIN=${domain} > common.env
-    echo TZ=\"$(cat /etc/timezone)\" >> common.env    
+    echo TZ=\"$(cat /etc/timezone)\" >> common.env
+    for d in */ ; do
+        cd ${d}
+        cp ../common.env .env
+        if test -f install.sh; then
+            install.sh
+        fi
+        cd ..
+    done
 }
 
 install_certbot ()
@@ -16,6 +24,6 @@ install_docker ()
    sudo usermod -aG docker ${USER}
 }
 
-set_common_env
+set_env
 install_certbot
 install_docker
