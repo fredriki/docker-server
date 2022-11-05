feea78eea68b217700adbc0fc83d149aa073ead2
fredriki <17081472+fredriki@users.noreply.github.com>
2022-11-05 16:03:55 +0100

    Added management script for cert
    Can do manual dns challenge.
    Also can update all images.

---
+++ manage.sh
@@ -0,0 +1,29 @@
+cert ()
+{
+    MYDOMAIN=$(cat common.env | grep MYDOMAIN | cut -d "=" -f2 | xargs)
+    echo "Fixing certificate for ${MYDOMAIN} :)"
+
+    sudo certbot certonly --manual --preferred-challenge dns -d ${MYDOMAIN} -d "*.${MYDOMAIN}"
+
+    sudo cp /etc/letsencrypt/live/${MYDOMAIN}/privkey.pem /etc/letsencrypt/live/${MYDOMAIN}/${MYDOMAIN}.key
+    echo "/etc/letsencrypt/live/${MYDOMAIN}/privkey.pem /etc/letsencrypt/live/${MYDOMAIN}/${MYDOMAIN}.key"
+    sudo cp /etc/letsencrypt/live/${MYDOMAIN}/fullchain.pem /etc/letsencrypt/live/${MYDOMAIN}/${MYDOMAIN}.crt
+    echo "/etc/letsencrypt/live/${MYDOMAIN}/privkey.pem /etc/letsencrypt/live/${MYDOMAIN}/${MYDOMAIN}.key"
+}
+
+update ()
+{
+    for d in */ ; do
+        cd ${d}
+        docker-compose pull
+        docker-compose up -d --remove-orphans
+        cd ..
+    done
+    docker image prune
+}
+
+case "$1" in
+    cert) cert;;
+    update) update;;
+esac
+
